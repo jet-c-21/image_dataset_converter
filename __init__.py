6535859dc185e7ca50032f53b5db45a72635228f
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2023-04-25 11:24:05 +0800

    update

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+"""
+Author: Jet C.
+GitHub: https://github.com/jet-c-21
+Create Date: 4/24/23
+"""
+from .coco_to_yolo.general_json2yolo import convert_coco_json
---
+++ coco_to_yolo/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+"""
+Author: Jet C.
+GitHub: https://github.com/jet-c-21
+Create Date: 4/24/23
+
+Reference: https://github.com/ultralytics/JSON2YOLO
+"""
+from .general_json2yolo import convert_coco_json
